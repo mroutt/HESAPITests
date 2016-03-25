4d73687ac73a015e5255255d36c46f423e2bacea
Mitch Routt <mrout@eidpassport.com>
2016-03-25 15:02:05 -0700

    Initial commit with a single happy path test.

---
+++ Tests/HESVisitorScanResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class HESVisitorScanResponse
+    {
+        private DateTime? _expDate = null;
+        public byte[] Photo { get; set; }
+        public string Firstname { get; set; }
+        public string Middlename { get; set; }
+        public string Lastname { get; set; }
+        public string Sponsorg { get; set; }
+        public DateTime? Expirationdate
+        {
+            get { return _expDate; }
+            set
+            {
+                if (value.HasValue)
+                    _expDate = value.Value.Date;
+            }
+        }
+        public string Status { get; set; }
+        public string Reason { get; set; }
+    }
+}
---
+++ Tests/ServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class ServiceTests
+    {
+
+        [Test]
+        public void CanGetGoodReturnFromHES()
+        {
+            var expectedExpirationDate = new DateTime(2016, 3, 31);
+            string expectedSponsorOrg = "TEST OFFICE SYMBOL 2";
+            string expectedStatus = "GREEN";
+
+            var response = PostBarcodeToService("TEMP0001003127");
+
+            Assert.AreEqual("CAMEY", response.Firstname);
+            Assert.AreEqual("ANDERSON", response.Lastname);
+            Assert.AreEqual(expectedExpirationDate, response.Expirationdate);
+            Assert.AreEqual(expectedStatus, response.Status);
+            Assert.AreEqual(expectedSponsorOrg, response.Sponsorg);
+            Assert.IsNotNull(response.Photo);
+        }
+
+        
+        private HESVisitorScanResponse PostBarcodeToService(string barcode)
+        {
+            var request = GetHESRequest(barcode);
+            return GetHESResponseFromRequest(request);
+        }
+        
+        private RestClient GetHESClient()
+        {
+            return new RestClient("http://app.huntinc.com/");
+        }
+
+        private RestRequest GetHESRequest(string barcode)
+        {
+            var request = new RestRequest("api/MAXCheck/CheckVisitor", Method.POST);
+            request.AddParameter("ScanData", barcode);
+            request.AddParameter("FacilityId", "PNDL");
+            request.AddParameter("ScanDateTime", DateTime.UtcNow.ToString());
+            request.AddParameter("IncludePii", "true");
+
+            return request;
+        }
+
+        private HESVisitorScanResponse GetHESResponseFromRequest(RestRequest request)
+        {
+            var client = GetHESClient();
+
+            var rawResponse = client.Execute(request).Content;
+
+            return JsonConvert.DeserializeObject<HESVisitorScanResponse>(rawResponse);
+        }
+    }
+}
